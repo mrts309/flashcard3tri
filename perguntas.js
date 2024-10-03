d697f7b82fd9b0af573285b5f1872f15a2b6599e
mrts309 <163892624+mrts309@users.noreply.github.com>
2024-10-03 10:06:21 -0300

    1

--- perguntas.js
+++ perguntas.js
@@ -1,7 +1,7 @@
 criaCartao(
-    'Programação',
-    'O que é Python?',
-    'O Python é uma linguagem de programação'
+    'Filmes',
+    'Qual nome do Batman?',
+    'Bruce Wayne'
 )
 
 criaCartao(
